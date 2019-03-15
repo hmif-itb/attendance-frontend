80254ac66dea993f7da81ef4772bcf83bbc07a94
jrandiny <jrandiny2@gmail.com>
2019-03-15 23:01:33 +0700

    Initial login page

---
+++ js/login.js
@@ -0,0 +1,32 @@
+function login(){
+    let loginUrl = localStorage.getItem('serverUrl');
+    if(loginUrl){
+        loginUrl += '/auth/login';
+
+        let postData = {
+            username: $('#username').val(),
+            password: $('#userPass').val()
+        };
+
+        fetch(loginUrl,{
+            method: 'POST',
+            headers: {
+                "Content-Type":"application/JSON"
+            },
+            body: JSON.stringify(postData)
+        }).then((val)=>{
+            val.json().then((content)=>{
+                if(content.jwt){
+                    sessionStorage.setItem('jwt',content.jwt);
+                    window.location.replace('event.html');
+                }
+            })
+        }).catch((err)=>{
+            console.error(err);
+            alert(err);
+        });
+    }else{
+        alert('Error, URL not supplied');
+    }
+
+}
