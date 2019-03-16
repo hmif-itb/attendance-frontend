0313ea0412158f2531fdefa10892ea205827686b
jrandiny <jrandiny2@gmail.com>
2019-03-17 01:03:14 +0700

    Add view all on status page

--- js/status.js
+++ js/status.js
@@ -2,6 +2,41 @@ let eventId = '';
 let serverUrl = '';
 let jwtKey = '';
 
+let lastNim = 0;
+let lastArr = [];
+
+function viewAll(){
+  notice('Loading','',false);
+
+  let namePromise = [];
+
+  lastArr.forEach(el => {
+    let nimUrl = serverUrl + '/students/'+el;
+    namePromise.push(fetch(nimUrl,{
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer '+jwtKey, 
+        'Content-Type':'application/JSON'
+      }
+    }));
+  });
+
+  Promise.all(namePromise).then((val)=>{
+    $('#listName').empty();
+    val.forEach(el=>{
+      el.json().then((userData)=>{
+        $('#listName').append('<p class="no-margin">'+userData[0].name+'</p>');
+        closeDialog('notice');
+        openDialog('list');
+        console.log(userData[0].name);
+      });
+    });
+  }).catch((err)=>{
+
+  });
+  
+}
+
 function updateCount(){
   let eventsUrl = serverUrl + '/events/'+eventId+'/attendances';
 
@@ -14,11 +49,35 @@ function updateCount(){
   }).then((val)=>{
     val.json().then((content)=>{
       if(val.status==200){
+        lastArr = content;
         $('#attendCount').text(content.length);
-        setTimeout(updateCount,2000);
+        let tempNim = content.pop();
+        if(lastNim!=tempNim){
+          lastNim = tempNim;
+  
+          let nimUrl = serverUrl + '/students/'+lastNim;
+  
+          fetch(nimUrl,{
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer '+jwtKey, 
+              'Content-Type':'application/JSON'
+            }
+          }).then((val)=>{
+            val.json().then((content)=>{
+              if(val.status==200){
+                $('#lastAttend').text(content[0].name);
+              }
+            });
+          }).catch((err)=>{
+            console.error(err);
+            alert(err);
+          });
+        }
+        setTimeout(updateCount,1000);
       }else{
         console.error(content);
-        alert(content);
+        alert(content.detail);
       }
     });
   }).catch((err)=>{
