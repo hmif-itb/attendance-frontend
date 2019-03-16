c6a7c684f5e90eb75e5d5525193125e78db7ffeb
jrandiny <jrandiny2@gmail.com>
2019-03-16 11:04:29 +0700

    Add event page

---
+++ js/event.js
@@ -0,0 +1,39 @@
+function eventPick(){
+    let eventId = $('#eventId').val();
+    if(eventId){
+        window.location.replace('attd.html#'+eventId);
+    }
+}
+
+
+$( document ).ready(function() {
+    let jwtKey = sessionStorage.getItem('jwt');
+
+    if(!jwtKey){
+        window.location.replace('/');
+    }
+
+    let serverUrl = localStorage.getItem('serverUrl');
+    let eventsUrl = serverUrl + '/events';
+    
+    fetch(eventsUrl,{
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer '+jwtKey, 
+            'Content-Type':'application/JSON'
+        }
+    }).then((val)=>{
+        val.json().then((content)=>{
+            if(val.status==200){
+                content.forEach(el => {
+                    $('#eventId').append('<option value="'+el.id+'">'+el.name+'</option>');
+                });
+            }else{
+                notice(content.detail,'OK',true);
+            }
+        });
+    }).catch((err)=>{
+        console.error(err);
+        alert(err);
+    });
+});
